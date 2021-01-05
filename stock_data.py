a8f817bb6ab2d80455382e00e9210105d3ca2464
DESKTOP-FS84GL7\jaceh <jaceholton@gmail.com>
2021-01-04 20:58:37 -0800

    Adding watchlist data from Alex. Adding functionality to scrap data from CSV files and get intraday prices from AlphaVantage

--- stock_data.py
+++ stock_data.py
@@ -3,6 +3,18 @@ import numpy as np
 import json
 import pandas as pd
 
+### DATA IMPORT ###
+#Import all stock data from Alex
+div_stocks = np.genfromtxt('C:/Users/jaceh/source/repos/HaceThings/TraderBot/StockData/Watchlist_DIV+Swings_2020_12_30.csv', delimiter=',', dtype=str)
+pref_stocks = np.genfromtxt('StockData/Watchlist_Preferred+Stocks_2020_12_30.csv', delimiter=',', dtype=str)
+risk_swing_stocks = np.genfromtxt('StockData/Watchlist_Risk+Swing_2020_12_30.csv', delimiter=',', dtype=str)
+
+symbols = []
+
+for rows in div_stocks:
+    symbols.append(rows[0])
+
+
 #API Key for Alpha Vantage (Need to move to a separate file)
 api_key = "QFAYI1XPUGD6UF9O"
 symbol = "IBM"
@@ -11,15 +23,27 @@ symbol = "IBM"
 def get_stock_data(symbol="T", interval="5min", type="TIME_SERIES_INTRADAY"): 
     """
     Gathers stock data from Alpha Vantage. 
-    Inputs: symbol = stock ticker, interval = time interval of intra data, type
+    Inputs: symbol = stock ticker
+            interval = time interval of intra data
+            type = 
     """
     res = requests.get("https://www.alphavantage.co/query?function=" + type + "&symbol=" + symbol + "&interval=" + interval  + "&apikey=" + api_key)
     data = json.loads(res.text)
-    #Insert dataframe here
-    print(data)
+
+    #Returns a group of the key-value pairs in the dictionary
+    results = data.items()
+    #Converts obj to a list
+    temp_data = list(result)
+    #Convert list to an array
+    temp_array = np.array(temp_data)
+
+    intraday_data = np.array(list(temp_array[1][1].items()))
+
+
 
 def main():
-    get_stock_data()
+    for sym in symbols:
+        get_stock_data(sym)
 
 if __name__ == '__main__': 
     main()
