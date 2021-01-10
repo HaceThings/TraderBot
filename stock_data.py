24789582555b0a09485f13b75fa6d069ea4df558
Jace <jaceholton@gmail.com>
2021-01-09 18:34:13 -0800

    Restructured program into a class that can be used to gather data to a SQL database.

--- stock_data.py
+++ stock_data.py
@@ -2,107 +2,125 @@ import requests
 import numpy as np
 import json
 import pandas as pd
+import sqlite3
 
 ### DATA IMPORT ###
 #Import all stock data from Alex
 div_stocks = np.genfromtxt('StockData/Watchlist_DIV+Swings_2020_12_30.csv', delimiter=',', dtype=str, skip_header=1)
 pref_stocks = np.genfromtxt('StockData/Watchlist_Preferred+Stocks_2020_12_30.csv', delimiter=',', dtype=str, skip_header=1)
 risk_swing_stocks = np.genfromtxt('StockData/Watchlist_Risk+Swing_2020_12_30.csv', delimiter=',', dtype=str, skip_header=1)
-stock_lists = [div_stocks, pref_stocks, risk_swing_stocks]
-
-symbols = []
-
-for x in range(0,len(stock_lists)):
-    for rows in stock_lists[x]:
-        temp = []
-        temp.append(rows[0])
-        if x == 0:
-            temp.append("Dividend")
-        elif x == 1:
-            temp.append("Preferred")
-        elif x == 2:
-            temp.append("Risk Swing")
-        else:
-            temp.append("IDK")
-        symbols.append(temp)
-
-df = pd.DataFrame(symbols)
-
-import sqlite3
-conn = sqlite3.connect('intraday_data.db')
-c = conn.cursor()
-conn.commit()
-df.to_sql('STOCK_MAIN', conn, if_exists='replace', index = False)
+stock_type_lists = [div_stocks, pref_stocks, risk_swing_stocks]
 
 
+class data_collector():
+    def __init__(self):
+        self.symbol_list = []
+        self.db_filename = 'data.db'
+        self.api_key = "QFAYI1XPUGD6UF9O"
 
-#API Key for Alpha Vantage (Need to move to a separate file)
-api_key = "QFAYI1XPUGD6UF9O"
-symbol = "IBM"
+    def create_db(self):
+        try:
+            conn = sqlite3.connect(self.db_filename)
+            c = conn.cursor()
+            # Create a table to store stock information
+            c.execute('CREATE TABLE STOCK_INFO (Symbol text, Sector text, DividendYield decimal, DividendDate datetime, ExDividendDate datetime')
+            # Create a table to store stock intraday data
+            c.execute('CREATE TABLE STOCK_DATA (TimeStamp datetime, Open decimal, High decimal, Low decimal, Close decimal, Volume int)')
+            conn.commit()
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
 
-# Gathers stock data from Alpha Vantage. 
-def get_stock_data(symbol="T", interval="5min", type="TIME_SERIES_INTRADAY"): 
-    """
-    Gathers stock data from Alpha Vantage. 
-    Inputs: symbol = stock ticker
-            interval = time interval of intra data
-            type = 
-    """
-    res = requests.get("https://www.alphavantage.co/query?function=" + type + "&symbol=" + symbol + "&interval=" + interval  + "&apikey=" + api_key)
-    convert_request_data(res, symbol)
+    def get_db_filename(self):
+        return self.db_filename
 
-def convert_request_data(res, symbol):
-    time_stamps = []
-    open = []
-    high = []
-    low = []
-    close = []
-    volume = []
-    headers = ["Time Stamp", "Open", "High", "Low", "Close", "Volume"]
+    def set_db_filename(self, db_filename):
+        self.db_filename = db_filename
+
+    def get_symbol_list(self):
+        return self.symbol_list
+
+    def set_symbol_list(self, symbol_list):
+        self.symbol_list = symbol_list
+
+    def get_stock_information(self, symbol):
+         res = requests.get("https://www.alphavantage.co/query?function=" + "OVERVIEW" + "&symbol=" + symbol + "&apikey=" + self.api_key)
+         data = json.loads(res.text)
+         result = data.items()
+         temp_data = list(result)
+         temp_array = np.array(temp_data)
+         print(temp_array)
+
+
+    def get_stock_time_series_data(self, symbol="T", interval="1min", type="TIME_SERIES_INTRADAY"): 
+        """
+        Gathers stock intraday data from AlphaVantage. 
+        Parameters: 
+            symbol = stock symbol to gather
+            interval = time interval between two consecutive data points in the time series. The following values are supported: 1min, 5min, 15min, 30min, 60min.
+            type = the time series of your choice. Please see below for all options:
+                TIME_SERIES_INTRADAY_EXTENDED - This API returns the most recent 1-2 months of intraday data and is best suited for short-term/medium-term charting and trading strategy development.
+                TIME_SERIES_DAILY - This API returns historical intraday time series for the trailing 2 years, covering over 2 million data points per ticker. 
+                TIME_SERIES_DAILY_ADJUSTED - This API returns raw (as-traded) daily time series (date, daily open, daily high, daily low, daily close, daily volume) of the global equity specified, covering 20+ years of historical data. 
+                TIME_SERIES_WEEKLY - This API returns weekly time series (last trading day of each week, weekly open, weekly high, weekly low, weekly close, weekly volume) of the global equity specified, covering 20+ years of historical data. 
+                TIME_SERIES_WEEKLY_ADJUSTED - This API returns weekly adjusted time series (last trading day of each week, weekly open, weekly high, weekly low, weekly close, weekly adjusted close, weekly volume, weekly dividend) of the global equity specified, covering 20+ years of historical data. 
+                TIME_SERIES_MONTHLY - This API returns monthly time series (last trading day of each month, monthly open, monthly high, monthly low, monthly close, monthly volume) of the global equity specified, covering 20+ years of historical data. 
+                TIME_SERIES_MONTHLY_ADJUSTED - This API returns monthly adjusted time series (last trading day of each month, monthly open, monthly high, monthly low, monthly close, monthly adjusted close, monthly volume, monthly dividend) of the equity specified, covering 20+ years of historical data. 
+        """
+
+        res = requests.get("https://www.alphavantage.co/query?function=" + type + "&symbol=" + symbol + "&interval=" + interval  + "&apikey=" + self.api_key)
+
+        time_stamps = [], open = [], high = []
+        low = [], close = [], volume = []
+        stocks = [], stock_types = []
+        headers = ["Time Stamp", "Open", "High", "Low", "Close", "Volume"]
+
+        try:
+            conn = sqlite3.connect(self.db_filename)
+            c = conn.cursor()
+
+            data = json.loads(res.text)
+            ###Conversion from dictionary to pandas dataframe###
+            #Returns a group of the key-value pairs in the dictionary
+            result = data.items()
+            #Converts obj to a list
+            temp_data = list(result)
+            #Convert list to an array
+            temp_array = np.array(temp_data)
+            intraday_data = np.array(list(temp_array[1][1].items()))
     
-    conn = sqlite3.connect('intraday_data.db')
-    c = conn.cursor()
-    try:
-        data = json.loads(res.text)
-        ###Conversion from dictionary to pandas dataframe###
-        #Returns a group of the key-value pairs in the dictionary
-        result = data.items()
-        #Converts obj to a list
-        temp_data = list(result)
-        #Convert list to an array
-        temp_array = np.array(temp_data)
-        intraday_data = np.array(list(temp_array[1][1].items()))
+            for row in intraday_data:
+                stocks.append(symbol)
+                stock_types.append(symbol_type)
+                time_stamps.append(row[0])
+                open.append(row[1]['1. open'])
+                high.append(row[1]['2. high'])
+                low.append(row[1]['3. low'])
+                close.append(row[1]['4. close'])
+                volume.append(row[1]['5. volume'])
     
-        for row in intraday_data:
-            time_stamps.append(row[0])
-            open.append(row[1]['1. open'])
-            high.append(row[1]['2. high'])
-            low.append(row[1]['3. low'])
-            close.append(row[1]['4. close'])
-            volume.append(row[1]['5. volume'])
-    
-        df = pd.DataFrame(time_stamps, columns=[headers[0]])
-        df[[headers[1]]] = open
-        df[[headers[2]]] = high
-        df[[headers[3]]] = low
-        df[[headers[4]]] = close
-        df[[headers[5]]] = volume
-        print(df)
+            df = pd.DataFrame(stocks, columns=['Symbols'])
+            df[['Stock Type']] = stock_types
+            df[[headers[0]]] = time_stamps
+            df[[headers[1]]] = open
+            df[[headers[2]]] = high
+            df[[headers[3]]] = low
+            df[[headers[4]]] = close
+            df[[headers[5]]] = volume
+            print(df)
 
-
-        # TODO: Finish adding dynamic table adds for all symbols
-        c.execute('CREATE TABLE ' + symbol.capitalize() + ' (TimeStamp datetime, Open decimal, High decimal, Low decimal, Close decimal, Volume int)')
-        conn.commit()
-        print(symbol.capitalize())
-        df.to_sql(symbol.capitalize(), conn, if_exists='replace', index = False)
-        conn.close()
-    except Exception:
-        pass
-    
+            print(symbol.capitalize())
+            df.to_sql('STOCK_DATA', conn, if_exists='append', index = False)
+            conn.close()
+        except Exception:
+            pass
 
 def main():
-    for sym in symbols:
-        get_stock_data(sym[0])
+
+
 
 if __name__ == '__main__': 
     main()
